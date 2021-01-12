5e62ce429e9d2c08d306ed30aefb3b027c1cd35b
hengyunabc <hengyunabc@gmail.com>
2021-01-12 15:34:05 +0800

    remove lombok

--- protokit-generator/src/test/java/com/alibaba/protokit/gen/User.java
+++ protokit-generator/src/test/java/com/alibaba/protokit/gen/User.java
@@ -1,11 +1,25 @@
 package com.alibaba.protokit.gen;
 
-import lombok.Data;
-
 import java.io.Serializable;
 
-@Data
 public class User implements Serializable {
     private String name;
     private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
 }
